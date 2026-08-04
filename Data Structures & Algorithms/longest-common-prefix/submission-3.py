0b3f696b252f588aa9d59a4b7dbe723d7070b399
hana <117968852+HanaTeshome721@users.noreply.github.com>
2026-08-04 05:03:24 +0300

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+    #    hscanning
+        # prefix=strs[0]
+        # for i in range(1,len(strs)):
+        #     j=0
+        #     while j< min(len(prefix),len(strs[i])):
+        #         if prefix[j]!=strs[i][j]:
+        #             break
+        #         j+=1
+        #     prefix=prefix[:j]
+        # return  "".join(prefix)    
+
+    # v scanning
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i==len(s) or strs[0][i] != s[i]:
+                    return strs[0][:i]
+        return strs[0]  
+
+        # strs=sorted(strs) 
+        # for i in range(min(len(strs[0]) ,len(strs[-1]))):
+        #     if strs[0][i] != strs[-1][i]:
+        #         return strs[0][:i]
+        # return strs[0]        
+
+
+
+
+
